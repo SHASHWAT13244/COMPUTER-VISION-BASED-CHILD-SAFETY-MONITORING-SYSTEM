6e63e101bc48fc4915f62ffee6870cfac96e8122
SHASHWAT KHANDELWAL <shashwat.khandelwal2023@ssipmt.com>
2026-08-14 13:38:14 +0530

    Delete utlis directory

--- utlis/init.py
+++
@@ -1,8 +0,0 @@
-"""
-Utilities package for the Child Safety Monitoring System
-"""
-
-from .visualization import Visualizer
-from .video_processor import VideoProcessor
-
-__all__ = ['Visualizer', 'VideoProcessor']
